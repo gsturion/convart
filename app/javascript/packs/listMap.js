b7d5c32efe99ee081215bd97125e04f162380e0a
pedr0aroucha <pluccas500@gmail.com>
2020-11-26 15:01:05 -0300

    estilizando a home e adicionando o mapa

--- app/javascript/packs/listMap.js
+++ app/javascript/packs/listMap.js
@@ -13,7 +13,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-bienais.map(b => {
+bienais.forEach(b => {
   L.marker(b.coordinates).addTo(map)
     .bindPopup(b.name)
     .openPopup();
