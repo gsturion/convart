fc0a503cc98fe56551bb0091491b018a0d9a5deb
pedr0aroucha <pluccas500@gmail.com>
2020-11-26 14:42:59 -0300

    criando a páigna home e map

---
+++ app/javascript/packs/listMap.js
@@ -0,0 +1,21 @@
+const bienais = [
+  {
+    name: "Fundação Bienal de São Paulo - Avenida Pedro Álvares Cabral - Moema, São Paulo - SP", 
+    coordinates: [-23.5878111,-46.6557654]
+  },
+  {name: "Biennale, Venezia VE, Itália", coordinates: [45.4322409,12.3343608]},
+  {name: "Triennale di Milano, Viale Emilio Alemagna, Milano MI, Itália", coordinates: [45.4722367,9.1714183]},
+]
+
+var map = L.map('map').setView([-23.5878111,-46.6557654], 3);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+bienais.map(b => {
+  L.marker(b.coordinates).addTo(map)
+    .bindPopup(b.name)
+    .openPopup();
+  return b;
+})
